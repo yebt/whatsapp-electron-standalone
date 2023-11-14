a2b90fda71dac2fee6367c198d822b160f1ddc11
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-11-14 16:22:19 -0500

    remove old preload

--- preload.js
+++ preload.js
@@ -5,13 +5,13 @@
  *
  * https://www.electronjs.org/docs/latest/tutorial/sandbox
  */
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+// window.addEventListener('DOMContentLoaded', () => {
+//   const replaceText = (selector, text) => {
+//     const element = document.getElementById(selector)
+//     if (element) element.innerText = text
+//   }
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
+//   for (const type of ['chrome', 'node', 'electron']) {
+//     replaceText(`${type}-version`, process.versions[type])
+//   }
+// })
