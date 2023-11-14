66a8caf22529ce4dad1766694711d973afeab5fe
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-11-14 15:24:07 -0500

    Feat: start simple electron project

---
+++ renderer.js
@@ -0,0 +1,8 @@
+/**
+ * This file is loaded via the <script> tag in the index.html file and will
+ * be executed in the renderer process for that window. No Node.js APIs are
+ * available in this process because  is turned off and
+ *  is turned on. Use the contextBridge API in 
+ * to expose Node.js functionality from the main process.
+ */
+
